dd97a0bb4a33c0ef06076a5b4fd1b0919340f774
Kicpro <44734778+Kicpro@users.noreply.github.com>
2024-01-30 20:39:53 +0100

    Add files via upload

---
+++ back_side.bash
@@ -0,0 +1,50 @@
+﻿#!/bin/bash
+
+#
+# Variables
+#
+# Adjust these to match your DDOS Protected VPS & Backend IP's!
+#
+
+PROTECTED_IP="XX.XX.XX.XX"
+
+BACKEND_IP="XX.XX.XX.XX"
+
+#
+# DO NOT CHANGE ANYTHING BELOW THIS TEXT!
+#
+
+GATEWAY_IP=$(ip route show default 0.0.0.0/0 | awk '{print $3}')
+INTERFACE=$(ip -br addr show | grep $BACKEND_IP | awk '{print $1}')
+
+# Bring up GRE
+
+modprobe ip_gre
+lsmod | grep gre
+
+# Tunnel setup
+
+ip tunnel add gre1 mode gre local $BACKEND_IP remote $PROTECTED_IP ttl 255
+ip addr add 10.0.0.2/30 dev gre1
+ip link set gre1 up
+
+# New routes implementations
+
+echo '100 GRE' >> /etc/iproute2/rt_tables
+ip rule add from 10.0.0.0/30 table GRE
+ip route add default via 10.0.0.1 table GRE
+
+# Set DNS servers
+
+echo 'nameserver 1.1.1.1' > /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+
+# WARNING: This will cut all access to your BACKEND IP!
+
+ip route add $PROTECTED_IP via $GATEWAY_IP dev $INTERFACE onlink
+ip route replace default via 10.0.0.1
+
+# Sets proper MTU
+
+ip link set mtu 1440 gre0
+ip link set mtu 1440 gre1
---
+++ prot_side.bash
@@ -0,0 +1,54 @@
+﻿#!/bin/bash
+
+#
+# Variables
+#
+# Adjust these to match your DDOS Protected VPS & Backend IP's!
+#
+
+PROTECTED_IP="XX.XX.XX.XX"
+
+BACKEND_IP="XX.XX.XX.XX"
+
+#
+# DO NOT CHANGE ANYTHING BELOW THIS TEXT!
+#
+
+# Making sure iptables isn't blocking FORWARD
+
+iptables -P FORWARD ACCEPT
+
+# Enabling IP forwarding and ARP Proxy so we can forward our Protected IP
+
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.eth0.proxy_arp=1
+sysctl -p
+
+# Bring up GRE
+
+modprobe ip_gre
+lsmod | grep gre
+
+# Tunnel setup
+
+ip tunnel add gre1 mode gre local $PROTECTED_IP remote $BACKEND_IP ttl 255
+ip addr add 10.0.0.1/30 dev gre1
+ip link set gre1 up
+
+# NAT configuration
+
+iptables -t nat -A POSTROUTING -s 10.0.0.0/30 ! -o gre+ -j SNAT --to-source $PROTECTED_IP
+
+# Port forwarding
+
+iptables -A FORWARD -d 10.0.0.2 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -s 10.0.0.2 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+# Route our Protected IP through the Tunnel
+
+ip route add $PROTECTED_IP/32 via 10.0.0.2
+
+# Sets proper MTU
+
+ip link set mtu 1440 gre0
+ip link set mtu 1440 gre1
